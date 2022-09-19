d93aa940143ed1266f6395cc99288fe280bd0901
Max0490 <seagate90@mail.ru>
2022-09-19 21:47:43 +0300

    Initial commit

---
+++ src/main/java/guru/qa/HelloWorld.java
@@ -0,0 +1,22 @@
+package guru.qa;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String stringThatShouldBeDisplayedInConsole = "Hello, World!";
+
+        // must have
+        boolean flag = true;
+
+        byte aByte; // 8 бит
+        short aShort; // 16 бит
+
+        //must have
+        int aInt; // 32 бит
+        long aLong; //64 бит
+
+
+
+        System.out.println(stringThatShouldBeDisplayedInConsole);
+    }
+}
