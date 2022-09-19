07872d502113db5a645579c62e2aaf72212b200e
Max0490 <seagate90@mail.ru>
2022-09-19 22:03:43 +0300

    added info

--- src/main/java/guru/qa/HelloWorld.java
+++ src/main/java/guru/qa/HelloWorld.java
@@ -3,20 +3,32 @@ package guru.qa;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String stringThatShouldBeDisplayedInConsole = "Hello, World!";
+       // String stringThatShouldBeDisplayedInConsole = "Hello, World!";
 
         // must have
-        boolean flag = true;
+        //boolean flag = true;
 
-        byte aByte; // 8 бит
-        short aShort; // 16 бит
+        //byte aByte; // 8 бит
+        //short aShort; // 16 бит
 
         //must have
-        int aInt; // 32 бит
-        long aLong; //64 бит
+        //int aInt; // 32 бит
+
+        //long aLong; //64 бит
+       // float aFloat;
+        //must have
+       // double aDouble = 1.5;
+       // char aChar = '0'
 
 
+                //Операторы математические + - * / %
+                // Операторы условные > < >= <= == != !
+                // Операторы логические && - русское слово "И" || - русское слово "ИЛИ"
 
-        System.out.println(stringThatShouldBeDisplayedInConsole);
+        int a = 15;
+        int b = 10;
+
+
+        System.out.println(a + b);
     }
 }
