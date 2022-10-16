3ca644518609e89279c11f5f314ace30ff5edc76
USER <seagate90@mail.ru>
2022-10-16 12:16:58 +0400

    Java-2.1

--- src/main/java/guru/qa/Phone.java
+++ src/main/java/guru/qa/Phone.java
@@ -1,10 +1,18 @@
 package guru.qa;
 
+import com.sun.source.doctree.SeeTree;
+
+import java.util.List;
+import java.util.Set;
+
 public class Phone {
     int price;
     String phoneName;
     boolean isAndroidPhone;
     boolean isInCase;
+    Set<String> supportedOsVersions;
+
+
 
 
     public Phone(int price, String phoneName, boolean isAndroidPhone) {
@@ -18,6 +26,14 @@ public class Phone {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public void setInCase(boolean inCase) {
+        isInCase = inCase;
+    }
+
+    public void setSupportedOsVersions(Set<String> supportedOsVersions) {
+        this.supportedOsVersions = supportedOsVersions;
     }
 
     public void setPhoneName(String phoneName) {
--- src/main/java/guru/qa/PhoneExample.java
+++ src/main/java/guru/qa/PhoneExample.java
@@ -1,21 +1,45 @@
 package guru.qa;
 
+import java.util.List;
+import java.util.Set;
+
 public class PhoneExample {
 
     public static void main(String[] args) {
         Phone samsung = new Phone(900, "Samsung", true);
+        samsung.setSupportedOsVersions(Set.of("12", "13"));
+
         Phone iPhone = new Phone(1000, "iPhone", false);
+        iPhone.setSupportedOsVersions(Set.of("14", "15", "16"));
+
+        System.out.println("Supported OS count: " + iPhone.supportedOsVersions.size());
+
+        for (int i = 0; i < iPhone.supportedOsVersions.size(); i++) {
+            System.out.println("OS version: " + iPhone.supportedOsVersions[i]);
+        }
+
+        System.out.println("foreach");
+
+        for (String osVersion : iPhone.supportedOsVersions) {
+            System.out.println("OS version: " + osVersion);
+        }
+
+
+        int arrIndex = 0;
+        while (arrIndex < iPhone.supportedOsVersions.length) {
+            System.out.println("OS Version: " + iPhone.supportedOsVersions[arrIndex]);
+            arrIndex++;
+        }
+
+
         Phone sony = new Phone();
         sony.setPhoneName("Sony XPERIA");
         sony.setPrice(800);
         sony.setAndroidPhone(true);
-        iPhone.isInCase(true); //уточнить
+        //iPhone.isInCase(true); //уточнить
 
-
-
-
-        System.out.println(samsung.getPhoneName());
-        System.out.println(iPhone.getPhoneName());
+        //System.out.println(samsung.getPhoneName());
+      //  System.out.println(iPhone.getPhoneName());
 
     }
 }
